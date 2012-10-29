0face53313ced88c9c5d4345af956c0a9f75222c
Stanley Kwok <skwoka@gmail.com>
2012-10-28 19:59:46 -0700

    Updated homebrew cookbook to version 1.3.0

--- cookbooks/homebrew/libraries/homebrew_package.rb
+++ cookbooks/homebrew/libraries/homebrew_package.rb
@@ -3,11 +3,15 @@
 require 'chef/provider/package'
 require 'chef/resource/package'
 require 'chef/platform'
+require 'chef/mixin/shell_out'
 
 class Chef
   class Provider
     class Package
       class Homebrew < Package
+
+        include Chef::Mixin::ShellOut
+
         def load_current_resource
           @current_resource = Chef::Resource::Package.new(@new_resource.name)
           @current_resource.package_name(@new_resource.package_name)
@@ -40,11 +44,14 @@ class Chef
         end
 
         def current_installed_version
-          get_version_from_command("brew list --versions | awk '/^#{@new_resource.package_name} / { print $2 }'")
+          pkg = get_version_from_formula
+          versions = pkg.to_hash['installed'].map {|v| v['version']}
+          versions.join(" ") unless versions.empty?
         end
 
         def candidate_version
-          get_version_from_command("brew info #{@new_resource.package_name} | awk '/^#{@new_resource.package_name} / { print $2 }'")
+          pkg = get_version_from_formula
+          pkg.stable.version || pkg.version
         end
 
         def get_version_from_command(command)
@@ -52,21 +59,20 @@ class Chef
           version.empty? ? nil : version
         end
 
-        # Nicked from lib/chef/package/provider/macports.rb and tweaked
-        # slightly.
+        def get_version_from_formula
+          brew_cmd = shell_out!("brew --prefix")
+          libpath = ::File.join(brew_cmd.stdout.chomp, "Library", "Homebrew")
+          $:.unshift(libpath)
+
+          require 'global'
+          require 'cmd/info'
+
+          Formula.factory new_resource.package_name
+        end
+
         def get_response_from_command(command)
-          output = nil
-          status = popen4(command) do |pid, stdin, stdout, stderr|
-            begin
-              output = stdout.read
-            rescue Exception => e
-              raise Chef::Exceptions::Package, "Could not read from STDOUT on command: #{command}\nException: #{e.inspect}"
-            end
-          end
-          unless (0..1).include? status.exitstatus
-            raise Chef::Exceptions::Package, "#{command} failed - #{status.inspect}"
-          end
-          output
+          output = shell_out!(command)
+          output.stdout
         end
       end
     end
